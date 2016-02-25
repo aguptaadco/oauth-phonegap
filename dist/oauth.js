595fe788c5a8d118833b0e98a775dd46843edaa9
aguptaadco <abhishek@adcoprod.com>
2016-02-25 08:17:40 -0800

    Update oauth.js

--- dist/oauth.js
+++ dist/oauth.js
@@ -286,7 +286,7 @@ module.exports = function(Materia) {
           wnd.close();
         } catch (_error) {}
       }, 1200 * 1000);
-      wnd = window.open(url, "_self", 'toolbar=yes,location=no,closebuttoncaption=Back,presentationstyle=formsheet,toolbarposition=top,clearsessioncache=yes,clearcache=yes,allowinlinemediaplayback=YES');
+      wnd = window.open(url, "_self", 'toolbar=yes,closebuttoncaption=Back,presentationstyle=formsheet,toolbarposition=top,clearsessioncache=yes,clearcache=yes,allowinlinemediaplayback=YES');
       wnd.addEventListener("loadstart", function(ev) {
         var results;
         if (ev.url.substr(0, 17) !== "http://localhost/") {
